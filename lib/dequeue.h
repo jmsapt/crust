dd5c5e32149bf966c86ccbbbbde8a81f04124bcf
James Appleton <james.appleton01@gmail.com>
2024-07-07 12:46:10 +1000

    updated documentation

--- lib/dequeue.h
+++ lib/dequeue.h
@@ -13,11 +13,70 @@ struct Dequeue {
     unsigned int head;
 };
 
+/*
+ * Initializes the dequeue. This must be called on a new dequeue before the
+ * dequeue is used for the first time.
+ *
+ * Note, as buffers are lazily allocated, no allocations happen during this
+ * call. The first allocation will happen on the first push to the dequeue.
+ */
 void dequeue_init(struct Dequeue *q, size_t size);
-int dequeue_pop_tail(struct Dequeue *q, void *element);
+
+/* Gets a pointer to the ith element. This pointer can be used to mutate the
+ * element inplace, however it is only valid until a push to the dequeue (or
+ * destroying of the dequeue) occurs.
+ *
+ * Returns either a valid pointer (for a valid index i), or NULL for an invalid.
+ */
 void *dequeue_get(struct Dequeue *q, int i);
+
+/*
+ * Pops the tail element into the buffer pointed to by element, returning the
+ * length of the remaining dequeue. This only decrements the length.
+ *
+ * Returns -1 if the dequeue was empty and no element was popped.
+ */
+int dequeue_pop_tail(struct Dequeue *q, void *element);
+
+/*
+ * Pops the head element into the buffer pointed to by element, returning the
+ * length of the remaining dequeue. This decrements the length and moves the
+ * head index.
+ *
+ * Returns -1 if the dequeue was empty and no element was popped.
+ */
 int dequeue_pop_head(struct Dequeue *q, void *element);
+
+/*
+ * Pushes an element to the front of the dequeue by copying the data pointed to
+ * by element into the buffer, reallocating the buffer if needed. This is takes
+ * ammortized constant time.
+ *
+ * Returns the length of the new dequeue, or <0 on failure (i.e. failed to
+ * realloc the buffer).
+ */
 int dequeue_push_tail(struct Dequeue *q, void *element);
+
+/*
+ * Pushes an element to the front of the dequeue by copying the data pointed to
+ * element, reallocating the buffer if needed. This is takes ammortized constant
+ * time.
+ *
+ * Returns the length of the new dequeue, or <0 on failure (i.e. failed to
+ * realloc the buffer).
+ */
 int dequeue_push_head(struct Dequeue *q, void *element);
+
+/*
+ * Resizes the dequeue's buffer, copying all element into the new buffer which
+ * has a new capacity of old capacity + n.
+ *
+ * This operation takes linear time, proptortional to the len of the dequeue.
+ */
 int dequeue_grow(struct Dequeue *q, unsigned int n);
+
+/*
+ * Destroys the dequeue, freeing the associated buffer. This must be called on
+ * each dequeue before it falls out of scope.
+ */
 void dequeue_destroy(struct Dequeue *q);
--- lib/hashmap.h
+++ lib/hashmap.h
@@ -22,19 +22,37 @@ struct Bucket {
     void *key;
 };
 
-void hashmap_init(struct HashMap *m, const size_t key_size, const size_t val_size,
-                              unsigned long (*hash_fn)(const void *),
-                              int (*cmp_fn)(const void *a, const void *b));
+/* Initialises the hashmap given the sizes of the key/value, and associated
+ * hashing and comparision functions.
+ *
+ * Note that as the buffer is lazily allocated, there are no allocations that
+ * occur during the call.
+ */
+void hashmap_init(struct HashMap *m, const size_t key_size,
+                  const size_t val_size, unsigned long (*hash_fn)(const void *),
+                  int (*cmp_fn)(const void *a, const void *b));
 
+/*
+ * Destroys the hashmap, free all associated memory. Note, is user malloced
+ * memory is used in the keys or values, these allocations must be free by the
+ * user prior to calling destroy. This can be achieved by collecting the
+ * keys/values into a buffer then freeing them from that buffer.
+ *
+ * This function call takes linear time proportional to the capacity plus length
+ * of the map.
+ */
 void hashmap_destroy(struct HashMap *m);
 
-/* Inserts key into the hashmap, placing the bucket at the index return by the
+/*
+ * Inserts key into the hashmap, placing the bucket at the index return by the
  * hash function. Insertion is done by copying the value pointed to by both the
  * key and val pointers into the map.
  *
  * If the key already exists, then the value is overwritten and replace with the
  * new supplied value. This replacement is done inplace, so previous pointers to
  * this element will remain valid, and pointing to the new inserted value.
+ *
+ * This function call takes amortized constant time.
  *
  * Returns 0 on success, and <0 on error.
  */
@@ -43,27 +61,41 @@ int hashmap_insert(struct HashMap *m, const void *key, const void *val);
 /*
  * Removes the corresponding key-value pair from the map. If key does not
  * exists, then this has not effect on the map.
+ *
+ * This function call takes amortized constant time.
  */
 void hashmap_erase(struct HashMap *m, const void *key);
 
 /*
- * Returns a malloc-ed buffer filled with `len` number of keys. this buffer must
- * be freed before it falls out of scope.
+ * Copies keys and values into the respective buffers. These buffer must be at
+ * least the same length as the hashmap.
  *
- * These keys are copied out of the map, so they will/can outlive the hashmap
- * itself.
+ * If a NULL pointer is supplied to either key_buf or val_buf, then that buffer
+ * is skipped and no values are copied. For example, if key_buf is supplied but
+ * val_buf is NULL, only the keys are copied. If both are valid, both are
+ * copied, if neither is valid neither is copied.
+ *
+ * This function call takes linear time proportional to the map's length.
  */
-void *hashmap_keys(const struct HashMap *m);
+void hashmap_key_val(const struct HashMap *m, void *key_buf, void *val_buf);
 
-/* Returns the pointer to the value associated with the provided key. This
- * pointer can be used to mutate the value inplace.
+/*
+ * Returns the pointer to the value associated with the provided key. This
+ * pointer can be used to mutate the value inplace. It will also only live as
+ * long as the map itself.
  *
- * It will also only live as long as the map itself.
+ * Returns either a pointer to the element, or NULL if the element doesn't
+ * exist.
+ *
+ * This function call takes amortized constant time.
  */
 void *hashmap_get(const struct HashMap *m, const void *key);
+
 /*
  * Given a key, returns 1 if the key is contained in the map, or 0 if it is not
  * contained in the map.
+ *
+ * This function call takes amortized constant time.
  */
 int hashmap_contains(const struct HashMap *m, const void *key);
 
@@ -71,3 +103,6 @@ int hashmap_contains(const struct HashMap *m, const void *key);
 unsigned long hash_int(const void *key);
 unsigned long hash_long(const void *key);
 unsigned long hash_string(const void *key);
+
+/* ----- Provided Comparison Functions ----- */
+// TODO
